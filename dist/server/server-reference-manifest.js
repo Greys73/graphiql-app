c21eb61282890a44435adf9ca3361489c83a5259
Greys73 <v.myazin@gmail.com>
2023-12-05 20:53:09 +0400

    feat: clear Dist Tue, Dec  5, 2023  8:53:09 PM

--- dist/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"rGkn6ALJXtT/1QQZt+EpGE+EhiT8HxgfclDsAKU7TeY=\"\n}"
--- dist/transform.js
+++
@@ -1,6 +0,0 @@
-Error.stackTraceLimit = 100;
-global.self = global;
-require("./chunks/node_modules_0f997e._.js");
-require("./chunks/[root of the server]__ee00d3._.js");
-require("./chunks/[node]_dist_transform_d97283.js");
-require("./chunks/[node]_dist_transform_f1c3e7.js");
