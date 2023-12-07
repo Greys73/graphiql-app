3226238864daf0b25c6fae1ca5c8ea0eac986981
Duxcoder <iinegve_25@mail.ru>
2023-12-07 22:22:30 +0300

    feat: add theme with fonts

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  fonts: {
+    heading: 'var(--font-rubik)',
+    body: 'var(--font-rubik)',
+  },
+});
+
+export default theme;
