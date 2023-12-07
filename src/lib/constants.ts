65b348e34dced786f6f66520a32f5649590a99ca
Duxcoder <iinegve_25@mail.ru>
2023-12-07 22:21:10 +0300

    feat: add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,13 @@
+export enum NamePages {
+  Home = 'Welcome',
+  Editor = 'Editor',
+  SignUp = 'Sign Up',
+  Login = 'Login'
+}
+
+export enum PathPages {
+  Home = '/',
+  Editor = '/editor',
+  SignUp = '/signup',
+  Login = '/login'
+}
