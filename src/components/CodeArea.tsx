ed413a42a0468be34da1c4e9b806ddd152de1aa9
Greys73 <v.myazin@gmail.com>
2023-12-09 12:51:28 +0400

    feat: install Codemirror Sat, Dec  9, 2023 12:51:28 PM

---
+++ src/components/CodeArea.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+
+const CodeArea = () => {
+  const code = "console.log('Code Mirror!');"
+  return (
+    <CodeMirror
+      value={code}
+      height="100px"
+    />
+  );
+}
+export default CodeArea;
