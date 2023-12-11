d0b7927f4af19e2eb06a5a10bd2863f9ca38791b
Duxcoder <iinegve_25@mail.ru>
2023-12-11 23:23:52 +0300

    feat: update Header component with new Logo and Links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,45 @@
 'use client';
-import { Heading, Flex, Container } from '@chakra-ui/react';
-import { Link } from '@chakra-ui/next-js';
+import { Heading, Flex, Container, Button, Text } from '@chakra-ui/react';
+import { Image, Link } from '@chakra-ui/next-js';
+import Logo from '@src/assets/image/logo.svg';
+import AuthNavPanel from './AuthNavPanel';
 import { NamePages, PathPages } from '@src/lib/constants';
 
 const Header = () => {
   const pages = [
     { name: NamePages.Home, path: PathPages.Home },
     { name: NamePages.Editor, path: PathPages.Editor },
-    { name: NamePages.Login, path: PathPages.Login },
-    { name: NamePages.SignUp, path: PathPages.SignUp },
   ];
+
   return (
     <Flex as='header' align='center' w='100%' h={'100px'}>
-      <Container maxW='1080px'>
-        <Heading as='h1' size='2xl' noOfLines={1}>
-          Header
-        </Heading>
-        <Flex gap='10'>
+      <Container display={'flex'} alignItems={'center'} maxW='1080px' justifyContent={'space-between'}>
+        <Link
+          href={PathPages.Home}
+          _hover={{ textDecoration: 'none' }}
+          display='flex'
+          alignItems={'center'}
+          gap='4'
+        >
+          <Image src={Logo} boxSize='40px' alt='GraphQL' />
+          <Heading as='h1' size='md' noOfLines={1}>
+            <Text as={'span'} color='base.500'>
+              GraphQL
+            </Text>
+            Application
+          </Heading>
+        </Link>
+        <Flex gap='6' alignItems={'center'}>
           {pages.map((page) => {
             return (
-              <Link key={page.path} href={page.path} color='blue.400' _hover={{ color: 'blue.500' }}>
-                {page.name}
+              <Link key={page.path} href={page.path}>
+                <Button variant={'link'} fontSize={'16px'} fontWeight={'300'} color='link.100'>
+                  {page.name}
+                </Button>
               </Link>
             );
           })}
+          <AuthNavPanel />
         </Flex>
       </Container>
     </Flex>
