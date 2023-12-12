970772007f35dc9308b4d6fbac4170ee482dd089
Duxcoder <iinegve_25@mail.ru>
2023-12-12 23:16:31 +0300

    feat: create links card in welcome page with redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,69 @@
 'use client';
-import { Card, CardHeader, CardBody, CardFooter, Heading, Text, Button, Flex } from '@chakra-ui/react';
+import { Container, Heading, Flex, useToast } from '@chakra-ui/react';
+import { NamePages, PathPages } from '@src/lib/constants';
+import AuthCard from '@src/components/AuthCard';
+import { MdOutlineLogin, MdAppRegistration, MdRebaseEdit } from 'react-icons/md';
+import { useEffect, useState } from 'react';
 import { readUserSession } from '@src/lib/actions';
-import { PathPages } from '../lib/constants';
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { showErrorToast } from '@src/utils/toasts';
 
 export default function Home() {
-  const router = useRouter();
+  const [isAuthorized, checkAuthorized] = useState<boolean>();
+  const toast = useToast();
 
-  const checkAuth = async () => {
-    const { data } = await readUserSession();
-    if (!data.session) return router.push(PathPages.SignUp);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const checkAuthorization = async () => {
+    const { data, error } = await readUserSession();
+    checkAuthorized(() => Boolean(data.session));
+    if (error) showErrorToast(toast, error.message);
   };
+
   useEffect(() => {
-    checkAuth(), [checkAuth];
-  });
+    checkAuthorization();
+  }, [checkAuthorization]);
+
+  const cardsProps = {
+    editor: {
+      buttonText: NamePages.Editor,
+      buttonPath: PathPages.Editor,
+      text: `Our team has implemented a full-fledged GraphQL IDE!
+This is a fantastic developer tool to help you form queries and explore your Schema. Go to our online editor page, try, explore and implement your goals with us!`,
+      heading: 'Editor',
+      icon: MdRebaseEdit,
+    },
+    signUp: {
+      buttonText: NamePages.SignUp,
+      buttonPath: PathPages.SignUp,
+      text: `If you want to start using our application, then you need to register. We don’t feel sorry for it, it’s just what the customer demands. Don't worry, it won't take much time :)`,
+      heading: 'Yeap',
+      icon: MdAppRegistration,
+    },
+    login: {
+      buttonText: NamePages.Login,
+      buttonPath: PathPages.Login,
+      text: `Haven't seen you for a long time, go to the login page, I hope you haven't forgotten your
+              account information? Otherwise, you will have to re-register, because... We did not implement
+              the forgot password function :(`,
+      heading: 'Nope',
+      icon: MdOutlineLogin,
+    },
+  };
+
   return (
-    <Flex alignItems='center' flexDirection='column' mt='20' gap='10'>
-      <Card align='center' w='50%'>
-        <CardHeader>
-          <Heading size='md'> Example Title</Heading>
-        </CardHeader>
-        <CardBody>
-          <Text>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
-            in culpa qui officia deserunt mollit anim id est laborum.
-          </Text>
-        </CardBody>
-        <CardFooter>
-          <Button colorScheme='blue'>Example button</Button>
-        </CardFooter>
-      </Card>
-    </Flex>
+    <Container maxW='1080px' centerContent>
+      <Heading as='h1' size='2xl' my={14} p={2} textAlign={'center'}>
+        Welcome, are you new here?
+      </Heading>
+      <Flex gap={6} justifyContent={'center'} flexWrap={'wrap'}>
+        {isAuthorized ? (
+          <AuthCard {...cardsProps.editor} />
+        ) : (
+          <>
+            <AuthCard {...cardsProps.signUp} />
+            <AuthCard {...cardsProps.login} />
+          </>
+        )}
+      </Flex>
+    </Container>
   );
 }
--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,8 +1,17 @@
 import { UseFormRegisterReturn } from 'react-hook-form';
+import { type IconType } from 'react-icons';
 
 export interface AuthInputType {
   name: string;
   register: UseFormRegisterReturn;
   invalidMessage?: string;
   type: 'password' | 'email' | 'text';
+}
+
+export interface AuthCardType {
+  icon: IconType;
+  heading: string;
+  text: string;
+  buttonText: string;
+  buttonPath: string;
 }
