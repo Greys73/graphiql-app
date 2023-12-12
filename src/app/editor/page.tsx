0dd9ae72b1014f1b2fb33b6bd85c722660b1c75c
Duxcoder <iinegve_25@mail.ru>
2023-12-12 23:15:17 +0300

    feat: redirect in editor page to signUp

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,8 +1,21 @@
 'use client';
 import { Container, Heading } from '@chakra-ui/react';
-import { NamePages } from '@src/lib/constants';
+import { NamePages, PathPages } from '@src/lib/constants';
+import { readUserSession } from '@src/lib/actions';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Editor() {
+  const router = useRouter();
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const checkAuth = async () => {
+    const { data } = await readUserSession();
+    if (!data.session) return router.push(PathPages.SignUp);
+  };
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
   return (
     <Container maxW='1080px' centerContent>
       <Heading as='h1' size='4xl' p={2} noOfLines={1}>
