3c77c0d772f57111b580145eafaeb235a091328f
Greys73 <v.myazin@gmail.com>
2023-12-15 20:01:02 +0400

    feat: implement feat/develop changes Fri, Dec 15, 2023  8:01:02 PM

---
+++ src/app/editor/components/EditorPage.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+'use client';
+import { useEffect, useRef, useState } from 'react';
+import { GraphQLSchema } from 'graphql';
+import { graphql, updateSchema } from 'cm6-graphql';
+import { ReactCodeMirrorRef } from '@uiw/react-codemirror';
+import { Box, Card, CardBody, CardHeader, Container, Flex, Heading, Spacer, useToast } from '@chakra-ui/react';
+import CodeArea from '@src/components/CodeArea';
+import { TArea } from '@src/lib/types/types';
+import { NamePages } from '@src/lib/constants';
+import templateSchema from '@src/lib/templateSchema';
+import graphqlFormat from '@src/utils/graphql/graphqlFormat';
+import jsonFormat from '@src/utils/json/jsonFormat';
+import { EditorPageProps } from '@src/lib/types/types';
+import { showErrorToast } from '@src/utils/toasts';
+
+export default function Editor({ errorAuth }: EditorPageProps) {
+  const toast = useToast();
+  if (errorAuth) showErrorToast(toast, errorAuth.message);
+  // TODO: заменить на работу со store
+  const [schema, setSchema] = useState<GraphQLSchema>();
+  useEffect(() => {
+    templateSchema().then((newSchema) => {
+      if (newSchema) {
+        Object.values(areas).forEach((area) => {
+          const view = area.ref.current.view;
+          if (view) updateSchema(view, newSchema);
+        });
+        setSchema(newSchema);
+      }
+    });
+  }, []);
+
+  const areas: { [key: string]: TArea } = {
+    editor: { format: graphqlFormat, ref: useRef<ReactCodeMirrorRef>({}), extensions: [graphql(schema)] },
+    variables: { format: jsonFormat, ref: useRef<ReactCodeMirrorRef>({}) },
+    headers: { format: jsonFormat, ref: useRef<ReactCodeMirrorRef>({}) },
+    viewer: { readOnly: true, ref: useRef<ReactCodeMirrorRef>({}) },
+  };
+
+  return (
+    <Container maxW='1080px' centerContent>
+      <Heading as='h1' size='4xl' p={2} noOfLines={1}>
+        {NamePages.Editor}
+      </Heading>
+      <Flex w='100%'>
+        <Card w='48%' boxShadow='xl' m={2}>
+          <CardHeader>Editor</CardHeader>
+          <CardBody>
+            <CodeArea options={areas.editor} />
+          </CardBody>
+        </Card>
+        <Spacer />
+        <Card w='48%' boxShadow='xl' m={2}>
+          <CardHeader>Viewer</CardHeader>
+          <CardBody>
+            <CodeArea options={areas.viewer} />
+          </CardBody>
+        </Card>
+      </Flex>
+    </Container>
+  );
+}
--- src/lib/types/types.ts
+++ src/lib/types/types.ts
@@ -1,5 +1,48 @@
+import { AuthError, Session } from '@supabase/supabase-js';
+import { UseFormRegisterReturn } from 'react-hook-form';
+import { type IconType } from 'react-icons';
 import { ReactCodeMirrorProps, ReactCodeMirrorRef } from '@uiw/react-codemirror';
 import { MutableRefObject } from 'react';
+
+export interface AuthInputType {
+  name: string;
+  register: UseFormRegisterReturn;
+  invalidMessage?: string;
+  type: 'password' | 'email' | 'text';
+}
+
+export interface SubmitAuth {
+  email: string;
+  password: string;
+  passwordConfirm?: string;
+}
+
+export interface AuthInputPasswordType {
+  name: string;
+  register: UseFormRegisterReturn;
+  invalidMessage?: string;
+}
+
+export interface AuthCardType {
+  icon: IconType;
+  heading: string;
+  text: string;
+  buttonText: string;
+  buttonPath: string;
+}
+
+export interface CheckAuthProps {
+  isAuth: Session | null;
+  errorAuth: AuthError | null;
+}
+
+export interface WelcomePageProps extends CheckAuthProps {}
+export interface HeaderProps extends CheckAuthProps {}
+export interface AuthNavPanelProps extends CheckAuthProps {}
+
+export interface EditorPageProps {
+  errorAuth: AuthError | null;
+}
 
 export type TArea = {
   ref: MutableRefObject<ReactCodeMirrorRef>;
