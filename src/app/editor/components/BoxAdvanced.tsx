9328f723ea8dcd22129af955a5b071f2b0efe82d
Greys73 <v.myazin@gmail.com>
2023-12-17 11:18:40 +0400

    feat: delete Accordion tag Sun, Dec 17, 2023 11:18:40 AM

--- src/app/editor/components/BoxAdvanced.tsx
+++ src/app/editor/components/BoxAdvanced.tsx
@@ -1,12 +1,6 @@
 import { TAreas } from '@src/lib/types/types';
 import CodeArea from '@src/components/CodeArea';
 import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
   Tab,
   TabList,
   TabPanel,
@@ -16,34 +10,20 @@ import {
 
 function BoxAdvanced({ areas }: { areas: TAreas }) {
   return (
-    <Accordion allowToggle>
-      <AccordionItem>
-        <h2>
-          <AccordionButton bg='lightgrey' _expanded={{ bg: 'white' }}>
-            <Box as='span' flex='1' textAlign='left'>
-              Advanced
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-        </h2>
-        <AccordionPanel>
-          <Tabs isFitted variant='enclosed'>
-            <TabList>
-              <Tab>Variables</Tab>
-              <Tab>Headers</Tab>
-            </TabList>
-            <TabPanels h='100%'>
-              <TabPanel>
-                <CodeArea options={areas.variables} />
-              </TabPanel>
-              <TabPanel>
-                <CodeArea options={areas.headers} />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-        </AccordionPanel>
-      </AccordionItem>
-    </Accordion>
+    <Tabs isFitted variant='enclosed'>
+      <TabList>
+        <Tab>Variables</Tab>
+        <Tab>Headers</Tab>
+      </TabList>
+      <TabPanels h='100%'>
+        <TabPanel>
+          <CodeArea options={areas.variables} />
+        </TabPanel>
+        <TabPanel>
+          <CodeArea options={areas.headers} />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   );
 }
 
