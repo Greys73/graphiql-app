3b40e41b8a89db56ae561e8d8ca664a24407ee16
Duxcoder <iinegve_25@mail.ru>
2023-12-20 21:45:39 +0300

    feat: add animation for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,26 +6,72 @@ import Logo from '@src/assets/image/logo.svg';
 import AuthNavPanel from './AuthNavPanel';
 import { NamePages, PathPages } from '@src/lib/constants/pages';
 import { HeaderProps } from '@src/lib/types/types';
+import { useScroll, useTransform, motion, useAnimation, useMotionValueEvent } from 'framer-motion';
+
+const MotionFlex = motion(Flex);
+const MotionLink = motion(Link);
+const MotionImage = motion(Image);
+const MotionHeading = motion(Heading);
 
 const Header = ({ isAuth, errorAuth }: HeaderProps) => {
+  const { scrollY } = useScroll();
+  const scrollYRange = [0, 100, 100];
+
+  const containerHeight = useTransform(scrollY, scrollYRange, ['100px', '60px', '60px']);
+  const gapLogo = useTransform(scrollY, scrollYRange, ['1rem', '0.1rem', '0.1rem']);
+  const imageSize = useTransform(scrollY, scrollYRange, ['40px', '26px', '26px']);
+  const fontSize = useTransform(scrollY, scrollYRange, ['1.4rem', '1rem', '1rem']);
+
+  const controls = useAnimation();
+
+  useMotionValueEvent(scrollY, 'change', (val) =>
+    val > 100 ? controls.start('small') : controls.start('full')
+  );
+
   return (
-    <Flex as='header' align='center' w='100%' h={'100px'}>
+    <MotionFlex
+      as='header'
+      align='center'
+      w='100%'
+      h={'100px'}
+      position={'sticky'}
+      top={'0'}
+      backgroundColor={'white'}
+      zIndex={'1'}
+      animate={controls}
+      variants={{
+        full: { filter: 'drop-shadow(#fff 1px 2px 2px)' },
+        small: { filter: 'drop-shadow(rgba(204, 204, 204, 0.4) 1px 2px 2px)' },
+      }}
+      style={{
+        height: containerHeight,
+      }}
+    >
       <Container display={'flex'} alignItems={'center'} maxW='1080px' justifyContent={'space-between'}>
-        <Link
+        <MotionLink
           as={NextLink}
           href={PathPages.Home}
           _hover={{ textDecoration: 'none' }}
           display='flex'
           alignItems={'center'}
-          gap='4'
+          style={{
+            gap: gapLogo,
+          }}
         >
-          <Image src={Logo} boxSize='40px' alt='GraphQL' />
-          <Heading as='h1' size='md' noOfLines={1}>
+          <MotionImage src={Logo} boxSize={'40px'} style={{ height: imageSize }} alt='GraphQL' />
+          <MotionHeading
+            as='h1'
+            size='md'
+            noOfLines={1}
+            style={{
+              fontSize,
+            }}
+          >
             <Highlight query='GraphQL' styles={{ color: 'base.500' }}>
               GraphQL Application
             </Highlight>
-          </Heading>
-        </Link>
+          </MotionHeading>
+        </MotionLink>
         <Flex gap='6' alignItems={'center'}>
           <Link as={NextLink} key={PathPages.Home} href={PathPages.Home}>
             <Button variant={'link'} fontSize={'16px'} fontWeight={'300'} color='link.100'>
@@ -42,7 +88,7 @@ const Header = ({ isAuth, errorAuth }: HeaderProps) => {
           <AuthNavPanel isAuth={isAuth} errorAuth={errorAuth} />
         </Flex>
       </Container>
-    </Flex>
+    </MotionFlex>
   );
 };
 
