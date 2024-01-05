45a4603b6103ce96eddc9ea00ca41c8a0a0a088a
Greys73 <v.myazin@gmail.com>
2024-01-05 22:39:33 +0400

    feat: write tests Fri, Jan  5, 2024 10:39:33 PM

--- __tests__/app/editor/editor.test.tsx
+++ __tests__/app/editor/editor.test.tsx
@@ -1,38 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen } from '../../test-utils';
 import { describe, expect, test, vi } from 'vitest';
-import { Provider } from 'react-redux';
-import { CacheProvider } from '@chakra-ui/next-js';
-import { ChakraProvider, theme } from '@chakra-ui/react';
-import { setupStore } from '@src/store/store';
 import EditorPage from '@src/app/editor/components/EditorPage';
 
 vi.mock('cm6-graphql', () => {
-  return {
-    graphql: () => ({
-      extension: [],
-    }),
-  };
+  return { graphql: () => ({ extension: [] }) };
 });
 
 vi.mock('@codemirror/lang-json', () => {
-  return {
-    json: () => ({
-      extension: [],
-    }),
-  };
+  return { json: () => ({ extension: [] }) };
 });
 
 describe('PlayGround tests', () => {
-  test('test 1', () => {
-    render(
-      <Provider store={setupStore()}>
-        <CacheProvider>
-          <ChakraProvider resetCSS theme={theme}>
-            <EditorPage errorAuth={null} />
-          </ChakraProvider>
-        </CacheProvider>
-      </Provider>
-    );
+  test('is PlayGround displayed', () => {
+    render(<EditorPage errorAuth={null} />);
     expect(screen.getByText(/PlayGround/i)).toBeInTheDocument();
+  });
+  test('is all CodeAreas on page', () => {
+    render(<EditorPage errorAuth={null} />);
+    expect(screen.getAllByRole('textbox').length).toBe(4);
   });
 });
